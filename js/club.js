1a3358c46fb7c189c878184989c3b0031a2f7bf5
chanbonggg <22011705@sju.ac.kr>
2025-05-12 15:29:09 +0900

    디자인 개선 및 오타수정
    오타수정, 디자인 개선

--- js/club.js
+++ js/club.js
@@ -21,7 +21,7 @@ const clubs = [
     { name: "THE BLACK", desc: "2006년 설립된 중앙 보컬 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/TheBlack.jpg"},
     { name: "세종서회", desc: "세종대학교 중앙 서예 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/SejongCalligraphySociety.jpg"},
     { name: "인터페이스", desc: "중앙 학술 프로그래밍 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Interface.jpg"},
-    { name: "Prompt", desc: "AI 융합전자공학과 동아리로, 인공지능, 머신러닝, 임베디드 시스템에 관심 있는 학술 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Prompt.jpg"},
+    { name: "Prompt", desc: "AI 융합전자공학과 학술 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Prompt.jpg"},
     { name: "터벌림", desc: "세종대학교 중앙 풍물 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/Teobeollim.jpg"},
     { name: "MASTER", desc: "세종대학교 중앙 보드게임 동아리.", category: "봉사/친목/대외활동" ,img:"assets/images/club/master.jpg"},
     { name: "SJYC", desc: "세종대 중앙동아리 요트부.", category: "체육/레저" ,img:"assets/images/club/YachtClub.jpg"},
@@ -32,35 +32,35 @@ const clubs = [
     { name: "S.S.G", desc: "정보보호 학술 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/SSG.jpg"},
     { name: "RUSH", desc: "세종대 중앙 농구 동아리.", category: "체육/레저" ,img:"assets/images/club/rush.jpg"},
     { name: "SECLZ", desc: "세종대 클라이밍 동아리.", category: "체육/레저" ,img:"assets/images/club/Secles.jpg"},
-    { name: "UNSA", desc: "유엔한국협회(UNA) 산하 단체로, 국내 최대 연합 동아리 단체.", category: "봉사/친목/대외활동",img:"assets/images/club/UNSA.jpg"},
-    { name: "세종 로타랙트", desc: "국제 로타리클럽 산하 대학생 연합 봉사 동아리.", category: "봉사/친목/대외활동" ,img:"assets/images/club/SejongRotaract.jpg"},
+    { name: "UNSA", desc: "유엔한국협회(UNA) 산하 국내 최대 연합 동아리.", category: "봉사/친목/대외활동",img:"assets/images/club/UNSA.jpg"},
+    { name: "세종 로타랙트", desc: "대학생 연합 봉사 동아리.", category: "봉사/친목/대외활동" ,img:"assets/images/club/SejongRotaract.jpg"},
     { name: "SOULTRAIN", desc: "중앙 흑인 음악 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/SoulTrain.jpg"},
     { name: "밤부", desc: "세종대 사진 중앙 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/Bamboo.jpg"},
     { name: "세종투자연구회", desc: "세종대 주식투자 중앙 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/SejongInvestmentResearchSociety.jpg"},
     { name: "유마프렌", desc: "마케팅 중앙동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Yumafren.jpg"},
-    { name: "LIFRARY", desc: "생명과학과 관련된 여러 학술 활동과 정보 탐색 및 공유를 중점으로 하는 학술 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/LIFrary.jpg"},
+    { name: "LIFRARY", desc: "생명과학과 관련된 정보 탐색 및 공유를 중점으로 하는 학술 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/LIFrary.jpg"},
     { name: "RUDDER", desc: "로켓, RC항공기 제작 소모임.", category: "학술/전공 연계" ,img:"assets/images/club/Rudder.jpg"},
     { name: "Sound Plus", desc: "세종대학교 중앙 밴드 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/SoundPlus.jpg"},
     { name: "PAGE7", desc: "세종대 유일 중앙 락밴드 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/Page7.jpg"},
     { name: "TODO", desc: "세종대 액티비티 동아리.", category: "봉사/친목/대외활동" ,img:"assets/images/club/TODO.jpg"},
     { name: "SMARCLE", desc: "AI로봇학과 인공지능 응용 학술동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Smarcle.jpg"},
-    { name: "별무리", desc: "교내유일 천문관측 중앙동아리.", category: "학술/전공 연계계" ,img:"assets/images/club/Byeolmuri.jpg"},
+    { name: "별무리", desc: "교내유일 천문관측 중앙동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Byeolmuri.jpg"},
     { name: "STC", desc: "세종대 테니스 동아리.", category: "체육/레저" ,img:"assets/images/club/STC.jpg"},
-    { name: "MESH", desc: "전공 학술 발표, 스터디, 진로 세미나 등을 진행하는 나노신소재공학부 학술동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Mesh.jpg"},
+    { name: "MESH", desc: "나노신소재공학부 학술동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Mesh.jpg"},
     { name: "JUMP", desc: "세종대 스노우보드 중앙 동아리.", category: "체육/레저" ,img:"assets/images/club/JUMP.jpg"},
     { name: "TSP", desc: "세종대 탁구 중앙 동아리.", category: "체육/레저" ,img:"assets/images/club/TSP.jpg"},
     { name: "NinesiX", desc: "세종대학교 단과대 연합 댄스동아리.", category: "봉사/친목/대외활동" ,img:"assets/images/club/NineSix.jpg"},
-    { name: "Foogle", desc: "식품 산업을 탐구하고 연구하는 식품생명공학전공 유일한 학술동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Foogle.jpg"},
+    { name: "Foogle", desc: "식품생명공학전공 학술동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Foogle.jpg"},
     { name: "SAI", desc: "세종대학교 중앙 인공지능 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/SAI.jpg"},
     { name: "세종킹스", desc: "세종대 유일 중앙 야구 동아리.", category: "체육/레저" ,img:"assets/images/club/SejongKings.jpg"},
     { name: "한손", desc: "세종대학교 중앙 만화 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/Hansone.jpg"},
     { name: "유스 호스텔", desc: "세종대학교 중앙 여행 동아리.", category: "봉사/친목/대외활동" ,img:"assets/images/club/YouthHostel.jpg"},
-    { name: "아룸", desc: "기획, 디자인, 개발, 코딩테스트 준비 및 프로젝트를 중심으로 운영되는 학술 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Alom.jpg"},
+    { name: "아롬", desc: "코딩 및 프로젝트를 중심으로 운영되는 학술 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/Alom.jpg"},
     { name: "세콕세콕", desc: "세종대 배드민턴 동아리.", category: "체육/레저" ,img:"assets/images/club/SekokSekok.jpg"},
     { name: "그리디", desc: "IT 프로그래밍 동아리.", category: "학술/전공 연계" ,img:"assets/images/club/greedy.jpg"},
     { name: "두바퀴", desc: "세종대 오토바이 동아리.", category: "체육/레저" ,img:"assets/images/club/Dubaqwi.jpg"},
     { name: "MIS", desc: "세종대학교 중앙 문화·친목 동아리.", category: "봉사/친목/대외활동" ,img:"assets/images/club/mis.jpg"},
-    { name: "G:low", desc: "한국인, 외국인 학생들과 함께 한류에 대한 주제 탐구와 연구 및 문화 체험 활동을 하는 학술 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/Glow.jpg"}
+    { name: "G:low", desc: "한류에 대한 주제 탐구와 연구 및 문화 체험 활동을 하는 학술 동아리.", category: "공연/예술/문화" ,img:"assets/images/club/Glow.jpg"}
   ];
   clubs.sort((a, b) => {
     return a.name.localeCompare(b.name, 'ko-KR');
@@ -68,11 +68,17 @@ const clubs = [
   let currentCategory = "전체";
   let currentPage = 1;
   const itemsPerPage = 20;
-  
-  function filterClubs(category) {
+
+  function formatDesc(text) {
+  return text.replace(/\n/g, '<br>');
+}
+
+  function filterClubs(category,event) {
     currentCategory = category;
     currentPage = 1;
     renderClubs();
+    document.querySelectorAll('#filters button').forEach(btn => btn.classList.remove('active'));
+    event.target.classList.add('active');
   }
   
   function renderClubs(forcedList = null) {
@@ -88,7 +94,10 @@ const clubs = [
     const start = (currentPage - 1) * itemsPerPage;
     const end = start + itemsPerPage;
     const pageItems = filtered.slice(start, end);
-  
+    if (pageItems.length === 0) {
+      list.innerHTML = `<p style="grid-column: span 4; text-align:center; color:#888;">검색 결과가 없습니다.</p>`;
+      return;
+    }
     for (const club of pageItems) {
       const card = document.createElement("div");
       card.className = "club-card";
@@ -97,7 +106,7 @@ const clubs = [
           <img src="${club.img}" alt="${club.name}" class="club-image">
           <div class="club-text">
             <h3>${club.name}</h3>
-            <p>${club.desc}</p>
+            <p>${formatDesc(club.desc)}</p>
           </div>
         </div>
       `;
@@ -106,8 +115,12 @@ const clubs = [
   
     const totalPages = Math.ceil(filtered.length / itemsPerPage);
     document.getElementById("page-info").textContent = `${currentPage} / ${totalPages}`;
-    document.querySelector("#pagination button:first-child").disabled = currentPage === 1;
-    document.querySelector("#pagination button:last-child").disabled = currentPage === totalPages;
+    const prevBtn = document.querySelector("#pagination button:first-child");
+    const nextBtn = document.querySelector("#pagination button:last-child");
+
+    prevBtn.style.visibility = currentPage === 1 ? "hidden" : "visible";
+    nextBtn.style.visibility = currentPage === totalPages ? "hidden" : "visible";
+
   }
   
   function searchClubs() {
